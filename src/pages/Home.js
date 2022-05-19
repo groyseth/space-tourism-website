60b487cc591806f2596ca00100bf9890cb2e700c
Grant Royseth <groyseth@gmail.com>
2022-05-19 13:30:23 -0500

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,35 @@
 import React from 'react'
+
 import HomeNav from '../Components/HomeNav'
 import "../css/home.css"
-import image from "../assets/home/Group 16.png"
+import image from "../assets/home/Group.png"
 export default function Home() {
   return (
-    <body className='backGround'>
+    <div className='backGround'>
      {/* <HomeNav /> */}
      <nav className='navMain'>
        <ol className='navHome'>
          <li>00 Home</li>
-         <li>01 Crew</li>
+         <li>01 Destination</li>
+         <li>02 Crew</li>
+         <li>03 Technology</li>
        </ol>
      </nav>
      <section className='mainText'>
-  <h3>So, you want to travel to</h3>
+  <h3>SO, YOU WANT TO TRAVEL TO</h3>
   <h1>SPACE</h1>
   <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
   outer space and not hover kind of on the edge of it. Well sit back, and relax 
   because we’ll give you a truly out of this world experience!
 </p>
-  {/* Explore */}
- 
   </section>
-    </body>
+  <div>
+    <img src={image} alt='cirlce' className='circleImg'></img>
+    <h1 className='explore'>Explore</h1>
+  </div>
+    </div>
   )
 }
+{/* <Nav.Link href="#features">01 Destination</Nav.Link>
+      <Nav.Link href="#pricing">02 Crew</Nav.Link>
+      <Nav.Link href="#pricing">03 Technology</Nav.Link> */}
