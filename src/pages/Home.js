932f640f4070957915a08562b4e5ac9157fefe60
Grant Royseth <groyseth@gmail.com>
2022-05-19 11:49:28 -0500

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,13 @@ import image from "../assets/home/Group 16.png"
 export default function Home() {
   return (
     <body className='backGround'>
-     <HomeNav />
+     {/* <HomeNav /> */}
+     <nav className='navMain'>
+       <ol className='navHome'>
+         <li>00 Home</li>
+         <li>01 Crew</li>
+       </ol>
+     </nav>
      <section className='mainText'>
   <h3>So, you want to travel to</h3>
   <h1>SPACE</h1>
