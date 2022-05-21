3889e635d2633f8dcda31a2a115521c4f827cdd8
Grant Royseth <groyseth@gmail.com>
2022-05-20 20:33:53 -0500

    close call on giving myself a heart attack lol lmfao yea

--- src/pages/CrewPages/Douglas.js
+++ src/pages/CrewPages/Douglas.js
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import {crew} from "../../data"
+import pic from "../../assets/crew/image-douglas-hurley.png"
+import "../../css/crew.css"
 export default function Douglas() {
   return (
-    <div style={{color:"white"}}>Douglas</div>
+    <div className='navPlanets'>
+    {/* <h1 className='name'></h1> */}
+    <p className='discript'>{crew[0].description}</p>
+    <img src={pic} alt="" className="crewPic" />
+
+    <div className='pickDest'>
+      <h1 className='pickDestNum'>02</h1> <h1>MEET YOUR CREW</h1>
+    </div>
+    <div className='crewDis'>
+      <div className=''>
+         <h1 className='crewDisText'>{crew[0].bio}</h1>
+      </div>
+      {/* <div className='stats'>
+        <h1 className='distance'>{crew[0].travel}</h1>
+      </div> */}
+    </div>
+  </div>
   )
 }
