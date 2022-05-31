e0e846fdd81995a25f79d5ae384ad27251139274
Grant Royseth <groyseth@gmail.com>
2022-05-31 12:53:22 -0500

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
       {/* <NavTabs /> */}
 
       <section className='mainText' >
-        <h3>SO, YOU WANT TO TRAVEL TO</h3>
+        <div className='header'>SO, YOU WANT TO TRAVEL TO</div>
         <h1>SPACE</h1>
         <p>Let’s face it; if you want to go to space, you might as well genuinely go to
           outer space and not hover kind of on the edge of it. Well sit back, and relax
